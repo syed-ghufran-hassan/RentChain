25975e2938c79eec5b79af2596aff6ea108a8f2f
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-09-23 11:43:29 +0500

    added scripts for maintenance request and dispute resolution

--- RentChain/src/interfaces/IMaintenanceRequest.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IMaintenanceRequest {
-    function createRequest(address tenant, string calldata description) external returns (uint256);
-    function getRequest(uint256 requestId) external view returns (address, string memory, bool);
-}
